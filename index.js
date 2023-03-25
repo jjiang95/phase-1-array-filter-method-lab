27c728294f377ffd714fc7acd262620873126959
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-03-25 09:56:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    const matchingDrivers = array.filter(item => item.toLowerCase() === string.toLowerCase());
+    return matchingDrivers;
+}
+
+function fuzzyMatch(array, string) {
+    const matchingDrivers = array.filter(item => item.startsWith(string));
+    return matchingDrivers;
+}
+
+function matchName(array, string) {
+    const matchingDrivers = array.filter(item => item.name === string);
+    return matchingDrivers;
+}
